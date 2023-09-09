b42a44143d055f606ed51b135d799fc81e96fdd6
Akhil-Gupta18 <116026928+Akhil-Gupta18@users.noreply.github.com>
2023-09-09 20:29:43 +0530

    Time: 7 ms (90.78%), Space: 15.9 MB (71.35%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& v) {
-        vector<int> ans;
-	unordered_map<int,int> m;
+   int cnt1 = 0,cnt2 = 0;
+	int e1 = INT_MIN,e2 = INT_MIN;
 	int n = v.size();
 	for(int i : v)
 	{
-		m[i]++;
-		if(m[i]> n/3)
+		if(cnt1 == 0 and e2!=i)
 		{
-			ans.push_back(i);
-			m[i] = INT_MIN;
+			cnt1 = 1;
+			e1 = i;
+		}
+		else if(cnt2 == 0 and e1!=i)
+		{
+			cnt2 = 1;
+			e2 = i;
+		}
+		else if(e1 == i) cnt1++;
+		else if(e2 == i) cnt2++;
+		else{
+			cnt1--;
+			cnt2--;
 		}
 	}
-	return ans;
+        cout<<e1<<" "<<e2;
+	cnt1 = 0,cnt2 = 0;
+	for(int i : v)
+	{
+		if(e1 == i) cnt1++;
+		if(e2 == i) cnt2++;
+	}
+	int m = n/3 +1;
+	vector<int> k;
+	if(cnt1>=m) k.push_back(e1);
+	if(cnt2>=m) k.push_back(e2);
+	sort(k.begin(),k.end());
+	return k;
     }
 };
