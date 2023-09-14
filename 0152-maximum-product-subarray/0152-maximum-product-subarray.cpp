50fa04539d649749f9df456c2d453363b58586b9
Akhil-Gupta18 <116026928+Akhil-Gupta18@users.noreply.github.com>
2023-09-14 21:11:20 +0530

    Time: 0 ms (100.00%), Space: 14.1 MB (5.54%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -5,10 +5,12 @@ public:
 	int n = arr.size();
 	int prfix = 1;
 	int sffix = 1;
+    bool t = false;
 	for(int i = 0;i<n;i++)
 	{
 		if(prfix  == 0)
 		{
+            t = true;
 			prfix  = 1;
 		}
 		if(sffix == 0)
@@ -19,6 +21,8 @@ public:
 		sffix = sffix * arr[n-i-1];
 		maxi = max(maxi,max(prfix,sffix));
 	}
+        if(maxi < 0 and t)
+            return 0;
 	return maxi;
         
     }
