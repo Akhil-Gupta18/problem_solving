641a7231aba5cfb4f8d5241af804669d0674a5b9
Akhil-Gupta18 <116026928+Akhil-Gupta18@users.noreply.github.com>
2024-07-18 17:40:23 +0530

    Time: 3 ms (6.74%), Space: 42.6 MB (7.04%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) 
+    {
+        List<Integer> ans = new ArrayList<>();
+        if(root == null)
+            return ans;
+        Queue<Pair<TreeNode,Integer>> q = new LinkedList<>();
+        Map<Integer,Integer> map = new TreeMap<>();
+        q.add(new Pair<>(root,0));
+        while(!q.isEmpty())
+        {
+            int n = q.size();
+            for(int i = 0;i<n;i++)
+            {
+                Pair<TreeNode,Integer> p = q.poll();
+                TreeNode key = p.getKey();
+                int bfs = p.getValue();
+                map.put(bfs,key.val);
+                if(key.left!=null)
+                    q.add(new Pair<>(key.left,bfs+1));
+                if(key.right!=null)
+                    q.add(new Pair<>(key.right,bfs+1));
+            }
+        }
+        for(int i : map.values())
+        {
+            ans.add(i);
+        }
+        return ans;
+    }
+}
