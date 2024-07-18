02c42f9dfccd618e98ccf73fb106f3e50aa5d98a
Akhil-Gupta18 <116026928+Akhil-Gupta18@users.noreply.github.com>
2024-07-18 17:47:27 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (34.73%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -16,31 +16,45 @@
 class Solution {
     public List<Integer> rightSideView(TreeNode root) 
     {
+        // List<Integer> ans = new ArrayList<>();
+        // if(root == null)
+        //     return ans;
+        // Queue<Pair<TreeNode,Integer>> q = new LinkedList<>();
+        // Map<Integer,Integer> map = new TreeMap<>();
+        // q.add(new Pair<>(root,0));
+        // while(!q.isEmpty())
+        // {
+        //     int n = q.size();
+        //     for(int i = 0;i<n;i++)
+        //     {
+        //         Pair<TreeNode,Integer> p = q.poll();
+        //         TreeNode key = p.getKey();
+        //         int bfs = p.getValue();
+        //         map.put(bfs,key.val);
+        //         if(key.left!=null)
+        //             q.add(new Pair<>(key.left,bfs+1));
+        //         if(key.right!=null)
+        //             q.add(new Pair<>(key.right,bfs+1));
+        //     }
+        // }
+        // for(int i : map.values())
+        // {
+        //     ans.add(i);
+        // }
+        // return ans;
         List<Integer> ans = new ArrayList<>();
         if(root == null)
             return ans;
-        Queue<Pair<TreeNode,Integer>> q = new LinkedList<>();
-        Map<Integer,Integer> map = new TreeMap<>();
-        q.add(new Pair<>(root,0));
-        while(!q.isEmpty())
-        {
-            int n = q.size();
-            for(int i = 0;i<n;i++)
-            {
-                Pair<TreeNode,Integer> p = q.poll();
-                TreeNode key = p.getKey();
-                int bfs = p.getValue();
-                map.put(bfs,key.val);
-                if(key.left!=null)
-                    q.add(new Pair<>(key.left,bfs+1));
-                if(key.right!=null)
-                    q.add(new Pair<>(key.right,bfs+1));
-            }
-        }
-        for(int i : map.values())
-        {
-            ans.add(i);
-        }
-        return ans;
+        recursionRight(root,0,ans);
+        return  ans;
+    }
+    public void recursionRight(TreeNode root,int lev,List<Integer> ans)
+    {
+        if(root == null)
+            return;
+        if(ans.size()== lev)
+            ans.add(root.val);
+        recursionRight(root.right,lev+1,ans);
+        recursionRight(root.left,lev+1,ans);
     }
 }
